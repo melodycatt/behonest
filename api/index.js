ee13f41d4687d1edc6d6dd4ab049ba75d1f0d255
Melody <edward.lenzner@gmail.com>
2024-08-29 08:46:44 +1000

    what

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ app.use(cors())
 app.use(express.static(path.join(__dirname,'..', 'public')));
 
 app.get('/', (req, res) => {
+  console.log("hi")
   res.sendFile(path.join(__dirname, '..', 'public', 'verify.html'));
 });
 
